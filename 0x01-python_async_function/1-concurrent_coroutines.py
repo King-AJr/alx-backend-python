5c5d2203709b27bff7a54cab86ce33ad515f5e38
King-AJr <talk2king.aj@gmail.com>
2023-08-07 14:03:02 +0100

    spawn task_wait_random n times

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,9 @@ n times with the specified max_delay.
 """
 from typing import List
 import asyncio
-wait_random = __import__('0-basic_async_syntax').wait_random
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random n times"""
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,12 +6,14 @@ measures the total execution time
 """
 import asyncio
 import time
-wait_n = __import__('1-concurrent_coroutines').wait_n
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
 
 def measure_time(n: int, max_delay: int) -> float:
-     """calculates time it takes to run once cycle wait_n"""
-     s = time.perf_counter()
-     asyncio.run(wait_n(n, max_delay))
-     elapsed = time.perf_counter() - s
-     single_cycle: float = elapsed / n
-     return single_cycle
+    """calculates time it takes to run once cycle wait_n"""
+    s = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    elapsed = time.perf_counter() - s
+    single_cycle: float = elapsed / n
+    return single_cycle
