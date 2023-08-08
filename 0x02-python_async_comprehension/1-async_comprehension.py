5c879a48aa926751c03ba818846cda779ff01c40
King-AJr <talk2king.aj@gmail.com>
2023-08-08 15:14:25 +0100

    return total time it takes run async_comprehension 4 times

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,6 +8,7 @@ then return the 10 random numbers.
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
-async def async_comprehension() ->List[float]:
+
+async def async_comprehension() -> List[float]:
     """take a generator and return a list"""
     return [i async for i in async_generator()]
